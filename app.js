79daa7bc5f62645d78f7ae671761b6fc2ac31c8e
Talha <talhaandiz@gmail.com>
2024-08-22 08:52:05 +0300

    remove handlebars and node-cache

--- app.js
+++ app.js
@@ -2,39 +2,43 @@ require('dotenv').config();
 const express = require('express');
 const httpProxy = require('http-proxy');
 const morgan = require('morgan');
-const exphbs = require('express-handlebars'); 
 
 const app = express();
 const proxy = httpProxy.createProxyServer({});
 
-const targetUrl = process.env.TARGET_URL || 'http://localhost:3000';
+const targetUrl = process.env.TARGET_URL || 'http://localhost:5984';
 const port = process.env.PORT || 8000;
-
-app.engine('handlebars', exphbs.engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
 
 app.use(morgan('combined'));
 
+// CORS ayarları ve preflight request'leri işleme
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200); // OPTIONS request'leri burada sonlandır
+  }
   next();
 });
 
+const proxyOptions = {
+  target: targetUrl,
+  timeout: 5000,
+  proxyTimeout: 5000,
+  changeOrigin: true
+};
+
 app.use((req, res) => {
-  proxy.web(req, res, { target: targetUrl }, (err) => {
-    if (err) {
-      console.error('Proxy hatası:', err);
-      res.status(500).render('error', { errorMessage: 'Proxy sunucusuna bağlanılamadı.' });
-    }
+  proxy.web(req, res, proxyOptions, (err) => {
+    console.error('Proxy hatası:', err);
+    res.status(500).json({ errorMessage: 'Proxy sunucusuna bağlanılamadı.' });
   });
 });
 
 proxy.on('error', (err, req, res) => {
   console.error('Proxy hatası:', err);
-  res.status(500).render('error', { errorMessage: 'Hedef sunucuya bağlanılamadı.' });
+  res.status(500).json({ errorMessage: 'Hedef sunucuya bağlanılamadı.' });
 });
 
 app.listen(port, () => {
